0336fc90c1309fd5eca7e75715d86a1e0d0315b2
ChrisKye <chris.m.kye@gmail.com>
2020-11-25 19:56:14 +0800

    processed raw dataper subject cleaned video list

--- Processing.R
+++ Processing.R
@@ -1,15 +1,24 @@
 library(R.matlab)
-library(eegkit)
+library(svMisc)
+library(data.table)
 
 fileList <- list.files(path="/Users/chriskye 1/Desktop/DEAP/data_preprocessed_matlab", 
             pattern = "*.mat", full.names=TRUE)
 
-lapply(fileList, function(x) {
-    numFile <- readMat(x)
-    numFile <- numFile[2] ##40 x 40 x 8064 (video x channel x data)
-    for (i in c(1:40))
-        trial <- numFile[i,1:32, ]
-        eegfft(trial, 128, 4.0, 45.0)
-    
+## 1 second epochs per video per channel for each participant
+processedRaw <- lapply(fileList, function(x) {
+    numFile <- readMat(x)                    ##40 x 40 x 8064 (video x channel x data)
+    numFile <- numFile$data[,1:32,-c(1:384)]  ##40 x 32 x 7680 (video x channel x data)
+
+    output <- array(dim = c(32, 128, 2400)) ##32, 128, 2400 (channel x time x epoch)
+    for (i in 1:32) {
+        progress(i, progress.bar = TRUE)
+        for(j in 1:40) {
+            for(k in 1:60) {
+                output[i, 1:128, (j-1)*60 + k] <- numFile[j,i,(k-1)*128 + c(1:128)]
+            }
+        }
+    }
+    return(output)
 })
 
